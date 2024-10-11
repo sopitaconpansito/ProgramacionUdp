20e4077f3fbf7a4dbcb6f4c47302bd0afba011b9
maxxee1 <maximiliano.adonis@gmail.com>
2024-10-11 11:11:08 -0300

    Nuevas consultas SQL

--- HTML/consultas.sql
+++ HTML/consultas.sql
@@ -4,19 +4,20 @@ CREATE TABLE IF NOT EXISTS Products (
     Price INTEGER,
     stock INTEGER DEFAULT 10,
     image_path TEXT
+    description TEXT
 );
 
-INSERT INTO Products (name, Price, stock, image_path) VALUES
-('Kryptonita 97%', 9000, 10, '/resources/Kryptonita97.webp'),
-('Simbionte Klyntar', 200, 10, '/resources/Simbionte.jpg'),
-('Sable oscuro', 300, 10, '/resources/SableOscuro.jpg'),
-('Gas del Joker', 400, 10, '/resources/GasJoker.webp'),
-('Guantelete 12 gemas', 500, 10, '/resources/Guantelete12.jpg'),
-('Casco Mandalor', 600, 10, '/resources/CascoBeskar.webp'),
-('Planos mansión Wayne', 700, 10, '/resources/PlanosMansion.webp'),
-('Escudo del capitan America', 800, 10, '/resources/EscudoRoto.jpg'),
-('Mapa Estelar', 900, 10, '/resources/MapaEstelar.webp'),
-('Lazo de la verdad', 1000, 10, '/resources/LazoWW.jpg'),
-('Batimovil', 1100, 10, '/resources/Batimovil.jpg'),
-('USB-Killer', 1200, 10, '/resources/UsbKiller.jpg');
+INSERT INTO Products (name, Price, stock, image_path, description) VALUES
+('Kryptonita 97%', 9000, 10, '/resources/Kryptonita97.webp', '10 grs. Kryptonita de laboratorio ultra pura.'),
+('Simbionte Klyntar', 200, 10, '/resources/Simbionte.jpg', 'Unico Especimen clase 3 mejorado geneticamente.'),
+('Sable oscuro', 300, 10, '/resources/SableOscuro.jpg', 'Fuente de poder ilimitada al portador.'),
+('Gas del Joker', 400, 10, '/resources/GasJoker.webp', 'Ultima version compatible con M79 y PGL-65.'),
+('Guantelete 12 gemas', 500, 10, '/resources/Guantelete12.jpg', 'Con ultima tegnologia adaptado a gemas multiversales.'),
+('Casco Mandalor', 600, 10, '/resources/CascoBeskar.webp', 'Hecho en Beskar, perteneciente a los neocruzados mandalorianos.'),
+('Planos mansión Wayne', 700, 10, '/resources/PlanosMansion.webp', 'Planos detallados de las habitaciones secretas y "sotano".'),
+('Escudo del capitan America', 800, 10, '/resources/EscudoRoto.jpg', 'Recuperado despues de los sucesos "End Game".'),
+('Mapa Estelar', 900, 10, '/resources/MapaEstelar.webp', 'Sofware de navegacion, BIG DATA de todo el universo observable.'),
+('Lazo de la verdad', 1000, 10, '/resources/LazoWW.jpg', 'Capaz de himnotizar y exponer deseos de quien sea sometido por este.'),
+('Batimovil', 1100, 10, '/resources/Batimovil.jpg', 'Nuevo prototipo con tegnologia para camuflaje de invisivilidad.'),
+('USB-Killer', 1200, 10, '/resources/UsbKiller.jpg', 'Condensadores 500M Volts. capaz de vulnerar cualquier hardware.');
 
